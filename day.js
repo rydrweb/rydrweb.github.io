1678294eb2b2306c7d13f29204e5c7866e9c119a
rydrweb <mombotsrs@gmail.com>
2025-01-31 10:08:34 -0500

    fix

--- day.js
+++ day.js
@@ -1,10 +1,1 @@
-function updateDay() {
-    const day = document.getElementById('Day');
-    const now = new Date();
-    const hours = now.getDay().toString().padStart(2, '0');
-    clock.textContent = `${day}`;
-}
 
-updateClock();
-
-setInterval(updateClock, 1000);
