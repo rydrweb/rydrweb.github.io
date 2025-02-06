f683a2c5fc2440c77ee3a2519d7f796441476b1f
rydrweb <mombotsrs@gmail.com>
2025-02-05 22:18:39 -0500

    Update day.js

--- day.js
+++ day.js
@@ -1,7 +1,1 @@
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const today = new Date().getDay();
-console.log(`${days[today]}`);
-
-// If you want to display it on your webpage:
-document.getElementById("dayDisplay").textContent = `${days[today]}`;
 
